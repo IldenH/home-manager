b57f3ba01b33669e63a14c00c4bdf7681af60b46
IldenH <IldenH.1@proton.me>
2024-03-03 14:22:27 +0100

    A bunch of stuff

---
+++ home.nix
@@ -0,0 +1,74 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    username = "ildenhnix";
+    homeDirectory = "/home/ildenhnix";
+    stateVersion = "23.11";
+  };
+
+  imports = [
+    ./programs/neovim.nix
+  ];
+
+  home.packages = with pkgs; [
+    # neovim
+    neofetch
+    firefox
+    vesktop
+    # discord
+    # (pkgs.discord.override {
+    #   withVencord = true;
+    # })
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "IldenH";
+    userEmail = "IldenH.1@proton.me";
+    # aliases = {
+    #   pu = "push";
+    #   co = "checkout";
+    #   cm = "commit";
+    # };
+    extraConfig = {
+      credential.helper = "store"; # I should probably switch to ssh
+    };
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  # home.file = {
+  #   # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+  #   # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+  #   # # symlink to the Nix store copy.
+  #   # ".screenrc".source = dotfiles/screenrc;
+
+  #   # # You can also set the file content immediately.
+  #   # ".gradle/gradle.properties".text = ''
+  #   #   org.gradle.console=verbose
+  #   #   org.gradle.daemon.idletimeout=3600000
+  #   # '';
+  # };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/ildenhnix/etc/profile.d/hm-session-vars.sh
+  #
+  # home.sessionVariables = {
+  #   EDITOR = "nvim";
+  # };
+
+  programs.home-manager.enable = true;
+}
