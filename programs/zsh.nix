3c83be6a9dac17697065921a8f7755408ed7df2d
IldenH <IldenH.1@proton.me>
2024-03-06 14:16:43 +0100

    Zsh neofetch and ex()

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -42,8 +42,33 @@
 			home = "cd ~/.config/home-manager";
 			nvimconf = "nvim ~/.config/nvim";
 		};
+		initExtra = ''
+			neofetch
+			ex = () {
+				if [ -f $1 ] ; then
+					case $1 in
+						*.tar.bz2) tar xjf $1 ;;
+						*.tar.gz)  tar xzf $1 ;;
+						*.bz2)     bunzip2 $1 ;;
+						*.rar)     ${pkgs.unrar-wrapper}/bin/unrar x $1 ;;
+						*.gz)      gunzip $1 ;;
+						*.tar)     tar xf $1 ;;
+						*.tbz2)    tar xjf $1 ;;
+						*.tgz)     tar xzf $1 ;;
+						*.zip)     ${pkgs.unzip}/bin/unzip $1 ;;
+						*.Z)       uncompress $1 ;;
+						*.7z)      ${pkgs.p7zip}/bin/7z x $1 ;;
+						*.deb)     ar x $1 ;;
+						*.tar.xz)  tar xf $1 ;;
+						*.tar.zst) unzstd $1 ;;
+						*)         echo "Could not extract $1"
+					esac
+				else
+					echo "$1 is not a valid file"
+				fi
+		}
+		'';
 	};
-
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
