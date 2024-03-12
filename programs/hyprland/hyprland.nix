e708fe1e3d7473bfdb2d98a86542857c741c71be
IldenH <IldenH.1@proton.me>
2024-03-12 22:22:58 +0100

    Hyprland config changes

--- programs/hyprland/hyprland.nix
+++ programs/hyprland/hyprland.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, inputs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 {
 	home.packages = with pkgs; [
@@ -18,7 +18,10 @@
 		xwayland.enable = true;
 
 		settings = {
-			monitor=",preferred,auto,auto";
+			monitor = [
+				"HDMI-A-2,preferred,auto,auto"
+				"DP-2,preferred,auto,auto"
+			];
 			
 			# Execute your favorite apps at launch
 			# exec-once = waybar & hyprpaper & firefox
@@ -27,7 +30,7 @@
 			"$fileManager" = "dolphin";
 			"$webBrowser" = "firefox";
 			"$discord" = "vesktop";
-			"$launcher" = "rofi -show drun";
+			"$launcher" = "rofi -show drun -show-icons";
 			
 			env = [
 				"XCURSOR_SIZE,24"
@@ -36,16 +39,12 @@
 			
 			input = {
 			    kb_layout = "no";
-			    # kb_variant =
-			    # kb_model =
-			    # kb_options =
-			    # kb_rules =
+			    kb_options = [
+						"caps:super"
+						# "shift:both_capslock" # doesn't work with multiple options set?
+					];
 			
 			    follow_mouse = 1;
-			
-			    touchpad = {
-			        natural_scroll = false;
-			    };
 			
 			    sensitivity = 0; 
 			};
@@ -54,8 +53,8 @@
 			    gaps_in = 5;
 			    gaps_out = 20;
 			    border_size = 2;
-			    "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-			    "col.inactive_border" = "rgba(595959aa)";
+					"col.active_border" = "rgba(${config.colorScheme.palette.base04}ee) rgba(${config.colorScheme.palette.base05}ee) 45deg";
+			    "col.inactive_border" = "rgba(${config.colorScheme.palette.base00}aa)";
 			    layout = "dwindle";
 			    allow_tearing = false;
 			};
@@ -63,31 +62,34 @@
 			decoration = {
 			    rounding = 10;
 			
-			    blur = {
-			        enabled = true;
-			        size = 3;
-			        passes = 1;
-			        vibrancy = 0.1696;
-			    };
-			
-			    drop_shadow = true;
-			    shadow_range = 4;
-			    shadow_render_power = 3;
-			    "col.shadow" = "rgba(1a1a1aee)";
+			#     blur = {
+			#         enabled = true;
+			#         size = 3;
+			#         passes = 1;
+			#         vibrancy = 0.1696;
+			#     };
+			blur.enabled = false;
+
+			#     drop_shadow = true;
+			#     shadow_range = 4;
+			#     shadow_render_power = 3;
+			#     "col.shadow" = "rgba(1a1a1aee)";
+			drop_shadow = false;
 			};
 			
-			animations = {
-			    enabled = true;
-			    bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-					animation = [
-						"windows, 1, 7, myBezier"
-			    	"windowsOut, 1, 7, default, popin 80%"
-			    	"border, 1, 10, default"
-			    	"borderangle, 1, 8, default"
-			    	"fade, 1, 7, default"
-			    	"workspaces, 1, 6, default"
-					];
-			};
+			# animations = {
+			#     enabled = true;
+			#     bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+			# 		animation = [
+			# 			"windows, 1, 7, myBezier"
+			#     	"windowsOut, 1, 7, default, popin 80%"
+			#     	"border, 1, 10, default"
+			#     	"borderangle, 1, 8, default"
+			#     	"fade, 1, 7, default"
+			#     	"workspaces, 1, 6, default"
+			# 		];
+			# };
+			animations.enabled = false;
 			
 			dwindle = {
 			    pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
@@ -115,14 +117,18 @@
 			"$mainMod" = "SUPER";
 			
 			bind = [
-				"$mainMod, Q, exec, $terminal"
-				"$mainMod, C, killactive,"
+				"$mainMod, W, killactive,"
 				"$mainMod, M, exit,"
-				"$mainMod, E, exec, $fileManager"
 				"$mainMod, V, togglefloating,"
-				"$mainMod, R, exec, $menu"
 				"$mainMod, P, pseudo," # dwindle
 				"$mainMod, J, togglesplit," # dwindle
+
+				# apps
+				"$mainMod, SPACE, exec, $launcher"
+				"$mainMod, E, exec, $fileManager"
+				"$mainMod, T, exec, $terminal"
+				"SHIFT_ALT, N, exec, $webBrowser"
+				"SHIFT_ALT, M, exec, $discord"
 
 				# Move focus with mainMod + arrow keys
 				"$mainMod, left, movefocus, l"
@@ -130,29 +136,27 @@
 				"$mainMod, up, movefocus, u"
 				"$mainMod, down, movefocus, d"
 
-				# Switch workspaces with mainMod + [0-9]
-				"$mainMod, 1, workspace, 1"
-				"$mainMod, 2, workspace, 2"
-				"$mainMod, 3, workspace, 3"
-				"$mainMod, 4, workspace, 4"
-				"$mainMod, 5, workspace, 5"
-				"$mainMod, 6, workspace, 6"
-				"$mainMod, 7, workspace, 7"
-				"$mainMod, 8, workspace, 8"
-				"$mainMod, 9, workspace, 9"
-				"$mainMod, 0, workspace, 10"
+				"CTRL, F1, workspace, 1"
+				"CTRL, F2, workspace, 2"
+				"CTRL, F3, workspace, 3"
+				"CTRL, F4, workspace, 4"
+				"CTRL, F5, workspace, 5"
+				"CTRL, F6, workspace, 6"
+				"CTRL, F7, workspace, 7"
+				"CTRL, F8, workspace, 8"
+				"CTRL, F9, workspace, 9"
+				"CTRL, F0, workspace, 10"
 
-				# Move active window to a workspace with mainMod + SHIFT + [0-9]
-				"$mainMod SHIFT, 1, movetoworkspace, 1"
-				"$mainMod SHIFT, 2, movetoworkspace, 2"
-				"$mainMod SHIFT, 3, movetoworkspace, 3"
-				"$mainMod SHIFT, 4, movetoworkspace, 4"
-				"$mainMod SHIFT, 5, movetoworkspace, 5"
-				"$mainMod SHIFT, 6, movetoworkspace, 6"
-				"$mainMod SHIFT, 7, movetoworkspace, 7"
-				"$mainMod SHIFT, 8, movetoworkspace, 8"
-				"$mainMod SHIFT, 9, movetoworkspace, 9"
-				"$mainMod SHIFT, 0, movetoworkspace, 10"
+				"$mainMod CTRL, 1, movetoworkspace, 1"
+				"$mainMod CTRL, 2, movetoworkspace, 2"
+				"$mainMod CTRL, 3, movetoworkspace, 3"
+				"$mainMod CTRL, 4, movetoworkspace, 4"
+				"$mainMod CTRL, 5, movetoworkspace, 5"
+				"$mainMod CTRL, 6, movetoworkspace, 6"
+				"$mainMod CTRL, 7, movetoworkspace, 7"
+				"$mainMod CTRL, 8, movetoworkspace, 8"
+				"$mainMod CTRL, 9, movetoworkspace, 9"
+				"$mainMod CTRL, 0, movetoworkspace, 10"
 
 				# Example special workspace (scratchpad)
 				"$mainMod, S, togglespecialworkspace, magic"
